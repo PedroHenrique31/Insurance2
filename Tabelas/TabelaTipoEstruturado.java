7b0e1a51d06adf5122558932a0e02c67c268e8a1
Pedro Henrique Carneiro de Araújo <pedrohcaraujo1@hotmail.com>
2021-08-10 12:18:46 -0300

    Adicionado metodo a classe TabelaTipoEstruturado,iniciando testes.

--- Tabelas/TabelaTipoEstruturado.java
+++ Tabelas/TabelaTipoEstruturado.java
@@ -2,7 +2,7 @@ package Tabelas;
 
 import java.util.ArrayList;
 
-public abstract class TabelaTipoEstruturado<T> {
+public abstract class TabelaTipoEstruturado<T extends TipoEstruturado> {
     ArrayList<T> Tabela=new ArrayList<T>();
     T campoConstante;
     public void adicionaNovo(T novo){
@@ -18,6 +18,12 @@ public abstract class TabelaTipoEstruturado<T> {
     public void modifica(int posicao,String nome){
         Tabela[posicao].nome=nome;
     }
+    public void CriaNovo(String nome){
+        T novo;
+        int ultimaPosicao=Tabela.size()-1;
+        novo=new T(nome,ultimaPosicao);
+        Tabela.add(novo);
 
+    }
 
 }
