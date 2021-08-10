9a3682c5e77c6e52e49b0c846621995fe111966e
Pedro Henrique Carneiro de Araújo <pedrohcaraujo1@hotmail.com>
2021-08-10 11:17:55 -0300

    TabelaTipoEstruturado agora utiliza generics e pode receber diversos tipos, podemos agroa especificar seu uso de forma semelhante a um arraylist.

--- Tabelas/TabelaTipoEstruturado.java
+++ Tabelas/TabelaTipoEstruturado.java
@@ -2,7 +2,22 @@ package Tabelas;
 
 import java.util.ArrayList;
 
-public abstract class TabelaTipoEstruturado< T extends TipoEstruturado> extends ArrayList {
+public abstract class TabelaTipoEstruturado<T> {
+    ArrayList<T> Tabela=new ArrayList<T>();
+    T campoConstante;
+    public void adicionaNovo(T novo){
+        Tabela.add(novo);
+    }
+    public T busca(int i){
+        T buscado=Tabela.get(i);
+        return buscado;
+    }
+    public ArrayList<T> listaTodos(){
+        return Tabela;
+    }
+    public void modifica(int posicao,String nome){
+        Tabela[posicao].nome=nome;
+    }
 
 
 }
