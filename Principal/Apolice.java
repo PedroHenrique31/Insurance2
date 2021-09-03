e5b7e93ae71221fcc874e86161b593db56cdc869
Pedro Henrique Carneiro de Araújo <pedrohcaraujo1@hotmail.com>
2021-09-03 12:53:04 -0300

    Adicionado atributos a classe Apolice.

--- Principal/Apolice.java
+++ Principal/Apolice.java
@@ -1,13 +1,32 @@
 package Principal;
+/**
+ *          Classe Apolice
+ *          Pertence ao pacote Principal e tem 23 atributos de varias classes,
+ *    incluindo quase todos do pacote Tabelas, é uma classe central que agrega
+ *    diversas outras na aplicação, ela varia de acordo com o tipo de produto segurado e
+ *    deve possuir tipos de coberturas e ações diferentes para tal(as vezes um generics aqui seria promissor).
+ *    */
+
+import Principal.Tabelas.Ramo;
+import Principal.Tabelas.MeioPagamento;
+import Principal.Tabelas.FormaPagamento;
+import Principal.Tabelas.Produtor;
+import Principal.Tabelas.Assistencia;
+import Principal.Tabelas.Seguradora;
 
 //Apolice poderia ser uma classe abstrata a depender do tipo de produto coberto
 public class Apolice {//Classe 2
     int ID_SEGURO;
-    String NumeroApolice;
+    String NumeroApolice,Regiao;
     //Datas de início de fim de vigencia e data atual
-    //Seguradora SeguradoraAnterior,SeguradoraAtual;
+    Seguradora SeguradoraAnterior,SeguradoraAtual;
+    Ramo ramo;
+    MeioPagamento meioPagamento;
+    FormaPagamento formaPagamento;
+    Produtor produtor;
+    Assistencia assistencia;
 
-    Funcionario Produtor;
-// Será que ramo deve ser um objeto? pelo mapeamento objeto relacional sim
+    //Classes do pacote Principal
+
 
 }
