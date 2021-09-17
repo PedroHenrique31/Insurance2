b7bd98ae365e5a0ccd1857b0cd175e89947da4ef
Pedro Henrique Carneiro de Araújo <pedrohcaraujo1@hotmail.com>
2021-09-17 12:54:10 -0300

    primeira implementacao de DadoSegurado ainda nao testada, mas sera em breve, ainda falta resolver as listagens.

--- Principal/Segurado/DadoDeSegurado.java
+++ Principal/Segurado/DadoDeSegurado.java
@@ -3,9 +3,10 @@ package Principal.Segurado;
 //import Principal.Tabelas.TipoEstruturado;
 
 public abstract class DadoDeSegurado {
-    int ID_SEGURADO;
+    protected int ID_SEGURADO;
     final int tamanhoMaximo=20;
     public DadoDeSegurado(int ID_SEGURADO){
         this.ID_SEGURADO=ID_SEGURADO;
     }
+
 }
--- Principal/Segurado/Dependente.java
+++ Principal/Segurado/Dependente.java
@@ -1,7 +1,36 @@
 package Principal.Segurado;
 
-public class Dependente {//Classe 6
-    int ID_SEGURADO;
+import java.util.Locale;
+
+public class Dependente extends DadoDeSegurado {//Classe 6
+
     String NomeDependente, EstadoCivil, Ocupacao, Parentesco;
+
+    public Dependente(String nome,String estadoCivil,String ocupacao,String parentesco,
+                      int ID_SEGURADO){
+        super(ID_SEGURADO);
+        this.NomeDependente=nome.toUpperCase(Locale.ROOT);
+        this.EstadoCivil=estadoCivil.toUpperCase(Locale.ROOT);
+        this.Ocupacao=ocupacao.toUpperCase(Locale.ROOT);
+        this.Parentesco=parentesco.toUpperCase(Locale.ROOT);
+
+    }
+    //getters: para caso seja preciso ler fora do pacote.
+        //NOTA: alguns atributos não terão setters, pois não faz sentido modificá-los, mas tem setters,e.g. parentesco.
+    public String getNomeDependente() {
+        return NomeDependente;
+    }
+
+    public String getEstadoCivil() {
+        return EstadoCivil;
+    }
+
+    public String getOcupacao() {
+        return Ocupacao;
+    }
+
+    public String getParentesco() {
+        return Parentesco;
+    }
 
 }
