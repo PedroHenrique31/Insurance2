db3adbe82cc8a701aa5e7f8b2858be4d75366e88
Pedro Henrique Carneiro de Araújo <pedrohcaraujo1@hotmail.com>
2021-09-17 14:39:00 -0300

    Segue a implementacao de Dependente.

--- Principal/Segurado/DadoDeSegurado.java
+++ Principal/Segurado/DadoDeSegurado.java
@@ -2,6 +2,13 @@ package Principal.Segurado;
 
 //import Principal.Tabelas.TipoEstruturado;
 
+/**
+ *          Classe DadoDeSegurado
+ *
+ *    Classe abstrata para representar os tipos que orbitam segurado e possuem relação 1:N com este.
+ *    Também possui um atributo chamado tamanhoMaximo que serve para restringir o tamanho de uma String
+ *    que possui tamanho fixado no Banco de Dados.
+ *    @author Pedro Henrique Carneiro de Araújo*/
 public abstract class DadoDeSegurado {
     protected int ID_SEGURADO;
     final int tamanhoMaximo=20;
@@ -9,4 +16,11 @@ public abstract class DadoDeSegurado {
         this.ID_SEGURADO=ID_SEGURADO;
     }
 
+    public int getID_SEGURADO() {
+        return ID_SEGURADO;
+    }
+
+    public void setID_SEGURADO(int ID_SEGURADO) {
+        this.ID_SEGURADO = ID_SEGURADO;
+    }
 }
--- Principal/Segurado/Dependente.java
+++ Principal/Segurado/Dependente.java
@@ -1,11 +1,16 @@
 package Principal.Segurado;
 
 import java.util.Locale;
-
+/**
+ *          Classe Dependente
+ *     É um tipo de DadoDeSegurado, logo ela orbita em torno de segurado e um segurado
+ *   pode ter N Dependentes.
+ *   */
 public class Dependente extends DadoDeSegurado {//Classe 6
 
     String NomeDependente, EstadoCivil, Ocupacao, Parentesco;
-
+    /**
+     * public Dependente: cria um dependente em um Segurado.*/
     public Dependente(String nome,String estadoCivil,String ocupacao,String parentesco,
                       int ID_SEGURADO){
         super(ID_SEGURADO);
